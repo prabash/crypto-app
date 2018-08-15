b2093e62ab828d901591cdcab994ff4801742b65
Prabash <prabash.darshana@gmail.com>
2018-08-15 23:27:13 +0530

    Added support for API calls

--- src/index.js
+++ src/index.js
@@ -1,7 +1,21 @@
 const electron = require('electron');
 const BrowserWindow = electron.remote.BrowserWindow;
+const axios = require('axios');
 
 const notifyBtn = document.getElementById('notifyBtn');
+var price = document.querySelector('h1');
+var targetPrice = document.getElementById('targetPrice');
+
+function getBTC() {
+    axios.get('https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC&tsyms=USD').
+        then(res => {
+            const cryptos = res.data.BTC.USD;
+            price.innerHTML = '$' + cryptos.toLocaleString('en');
+        })
+}
+
+getBTC();
+setInterval(getBTC, 30000);
 
 notifyBtn.addEventListener('click', function () {
     // frame false will remove the frame around the window (with min, max, close buttons)
